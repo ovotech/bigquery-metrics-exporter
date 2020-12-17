5771ab05e926a60f362b84d015f9dbd6fddea317
chris-brindley <67270396+chris-brindley@users.noreply.github.com>
2020-12-17 15:36:55 +0000

    Add secret manager integration (#1)

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ovotech/bigquery-metrics-extractor
 go 1.15
 
 require (
+	cloud.google.com/go v0.72.0
 	cloud.google.com/go/bigquery v1.14.0
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
@@ -16,5 +17,6 @@ require (
 	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
 	golang.org/x/tools v0.0.0-20201215192005-fa10ef0b8743 // indirect
 	google.golang.org/api v0.36.0
+	google.golang.org/genproto v0.0.0-20201204160425-06b3db808446
 	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0 // indirect
 )
--- pkg/config/errors.go
+++ pkg/config/errors.go
@@ -7,10 +7,10 @@ var (
 	ErrMissingDatadogAPIKey = errors.New("no Datadog API key configured")
 
 	// ErrMissingGcpProject is the error returned when the Config is missing the Google project ID
-	ErrMissingGcpProject     = errors.New("no GCP project ID configured")
+	ErrMissingGcpProject = errors.New("no GCP project ID configured")
 
 	// ErrMissingMetricPrefix is the error returned when the Config is missing a metric prefix
-	ErrMissingMetricPrefix   = errors.New("no metric name prefix configured")
+	ErrMissingMetricPrefix = errors.New("no metric name prefix configured")
 
 	// ErrMissingMetricInterval is the error returned when the Config is missing a metric collection interval
 	ErrMissingMetricInterval = errors.New("no metric collection interval configured")
