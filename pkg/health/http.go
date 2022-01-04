cb83b64ae290cb5d2ceb6a0bbb3aa7ded32fa442
Chris Brindley <chris.brindley@ovoenergy.com>
2022-01-04 15:55:31 +0000

    Add rudimentary healthcheck handler

---
+++ pkg/health/http.go
@@ -0,0 +1,40 @@
+package health
+
+import (
+	"encoding/json"
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
+
+type Status string
+
+const (
+	Ok = Status("OK")
+	Error = Status("Error")
+)
+
+// ServiceStatus holds information about the health of the bqmetricsd service
+type ServiceStatus struct {
+	Status Status `json:"status"`
+}
+
+// Handler will handle HTTP requests to the health endpoint
+func (hs ServiceStatus) Handler(w http.ResponseWriter, _ *http.Request) {
+	data, err := json.Marshal(hs)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	switch hs.Status {
+	case Ok:
+		w.WriteHeader(http.StatusOK)
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	if _, err = w.Write(data); err != nil {
+		log.Err(err).Msg("error when writing health http response")
+	}
+}
---
+++ pkg/health/http_test.go
@@ -0,0 +1,52 @@
+package health
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestServiceStatus_Handler(t *testing.T) {
+	type fields struct {
+		Status Status
+	}
+	type want struct {
+		status int
+		body string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   want
+	}{
+		{ "health ok", fields{Status: Ok}, want{200, "{\"status\":\"OK\"}"}},
+		{ "health fail", fields{Status: Error}, want{500, "{\"status\":\"Error\"}"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hs := ServiceStatus{
+				Status: tt.fields.Status,
+			}
+
+			req, err := http.NewRequest("GET", "/health", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(hs.Handler)
+
+			handler.ServeHTTP(rr, req)
+
+			if rr.Code != tt.want.status {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					rr.Code, tt.want.status)
+			}
+
+			if rr.Body.String() != tt.want.body {
+				t.Errorf("handler returned unexpected body: got %v want %v",
+					rr.Body.String(), tt.want.body)
+			}
+		})
+	}
+}
